47354bf73723a1e17291eddf65edbfd28cc42afc
joe_song <jsong@itbit.com>
2016-05-08 08:00:39 -0400

    yolo

--- api/app.py
+++ api/app.py
@@ -19,14 +19,28 @@ class Landmarks(Resource):
         if gps:
             landmark = mongo.db.landmarks.find_one({"location":{"$near":{"$geometry":{
                 "type":"Point","coordinates":[float(gps[0]),float(gps[1])]},"$maxDistance":10000000}}})
+            url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=1" \
+                  "&explaintext&exintro&titles="+'_'.join(landmark['name'].split(' '))+"&redirects=&formatversion=2"
+            response = urllib2.urlopen(url)
+            data = json.load(response)
+            extract = data['query']['pages']
             return jsonify({"location": landmark["location"],
                             "name": landmark["name"],
-                            "address": landmark["address"]})
+                            "address": landmark["address"],
+                            "excerpt": extract})
         elif name:
             landmark = mongo.db.landmarks.find_one({"name": {"$regex":  name}})
+
+            url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=1" \
+                  "&explaintext&exintro&titles="+'_'.join(landmark['name'].split(' '))+"&redirects=&formatversion=2"
+            print url
+            response = urllib2.urlopen(url)
+            data = json.load(response)
+            extract = data['query']['pages']
             return jsonify({"location": landmark["location"],
                             "name": landmark["name"],
-                            "address": landmark["address"]})
+                            "address": landmark["address"],
+                            "excerpt": extract})
         else:
             return jsonify({"result": "error, need name or gps"})
 
@@ -65,7 +79,6 @@ class Excerpt(Resource):
         return jsonify({"result": out})
 
 
-
 def remove_text_inside_brackets(t, brackets="()[]"):
     count = [0] * (len(brackets) // 2) # count open/close brackets
     saved_chars = []
@@ -87,7 +100,7 @@ api = Api(app)
 api.add_resource(Index, "/", endpoint="index")
 api.add_resource(Landmarks, "/api/landmarks", endpoint="landmarks")
 api.add_resource(Landmarks, "/api/landmarks/<list:gps>", endpoint="gps")
-api.add_resource(Landmarks, "/api/landmarks/name/<string:name>", endpoint="lname")
+api.add_resource(Landmarks, "/api/landmarks/name/<string:name>", endpoint="name")
 api.add_resource(Eateries, "/api/eateries/<string:name>", endpoint="ename")
 api.add_resource(Excerpt, "/api/excerpt/<string:search>", endpoint="search")
 
